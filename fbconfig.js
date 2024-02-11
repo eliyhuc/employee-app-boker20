1eb62d970c6dcf1f172b31a7eb6ff073407701f4
Eliyhu Chitrit <eliyhuc@college.labs>
2024-02-11 08:54:11 +0200

    first initial

---
+++ fbconfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAw_6-XbQJZRdHj_3MH8fwL6rkbCN-GBA",
+  authDomain: "carsapp-boker20.firebaseapp.com",
+  projectId: "carsapp-boker20",
+  storageBucket: "carsapp-boker20.appspot.com",
+  messagingSenderId: "156842163213",
+  appId: "1:156842163213:web:d7ecf049647c8c80cfe944"
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
